91ce6640ef80dd61a897ede233e810df069fa6a2
Sudhakar Y <suyeturi@osius.com>
2019-11-06 17:02:00 +0530

    test cases

--- demo/src/test/java/com/virtusa/test/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/virtusa/test/demo/DemoApplicationTests.java
@@ -1,13 +1,43 @@
 package com.virtusa.test.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
+	
+//	@Autowired
+//	private DemoApplication demoApplication;
 
 	@Test
-	void contextLoads() {
+	public void numberToWord() {
+		String words = "six thousand seven hundred eighty-nine";
+		String numberWord = DemoApplication.numberToWord(6789);
+		assertEquals(words, numberWord);
 	}
 
 }
